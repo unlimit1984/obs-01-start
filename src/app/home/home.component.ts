de890ae6ac5923c50d9853fbc505163a0fc228fb
Vladimir Vysokomornyi <unlimit_rmc@mail.ru>
2020-01-27 21:26:58 +0100

    Observables [175] - Add operators filter and map

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Observable, Subscription} from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -22,17 +23,27 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
-        if (count === 2) {
+        if (count === 6) {
           observer.complete();
         }
-        if (count > 3) {
+        if (count > 9) {
           observer.error(new Error('Count is greater than 3!'));
         }
         count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+    // customIntervalObservable.pipe(map((data: number) => {
+    //   return 'Round: ' + (data + 1);
+    // }));
+    // this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+    this.firstObsSubscription = customIntervalObservable.pipe(
+      filter(data => {
+        return data > 0;
+      }),
+      map((data: number) => {
+        return 'Round: ' + (data + 1);
+      })).subscribe(data => {
       console.log(data);
     }, error => {
       console.log(error);
